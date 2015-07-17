fb5057b278519284c46b7efbe0615e3ddc7fccbe
gvieiraschwade <guilherme.vieiraschwade@amadeus.com>
2015-07-17 22:52:28 +0200

    Translation done

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-function translate(){
+function translate(a,b,c){
+
+    var inputText = document.getElementById("inputArea");
+
+    var resultText = document.getElementById("result-text");
+    resultText.textContent = inputText.value.replace(/a|e|i|o|u/gi, '');
+
     var resultCard = document.getElementById("result-card");
     resultCard.removeAttribute("style");
     return false;
